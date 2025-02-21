7dac8872e2497a71d04d135d49ae3f747493b4d2
Larissa Muniz <larissampmuniz@gmail.com>
2025-02-21 00:51:53 -0300

    feat: add initial cart context and cart sheet components

--- src/app/[slug]/menu/[productId]/components/product-header.tsx
+++ src/app/[slug]/menu/[productId]/components/product-header.tsx
@@ -1,24 +1,23 @@
 "use client";
 
-import { Button } from "@/components/ui/button";
+import { Product } from "@prisma/client";
 import { ChevronLeftIcon, ScrollTextIcon } from "lucide-react";
 import Image from "next/image";
-import { Product } from "@prisma/client";
 import { useRouter } from "next/navigation";
+
+import { Button } from "@/components/ui/button";
 
 interface ProductHeaderProps {
   product: Pick<Product, "name" | "imageUrl">;
 }
 
 const ProductHeader = ({ product }: ProductHeaderProps) => {
-  
-    const router = useRouter();
-    const handleBackClick = () => router.back();
-
-    return (
-    <div className="relative w-full h-[300px] ">
+  const router = useRouter();
+  const handleBackClick = () => router.back();
+  return (
+    <div className="relative min-h-[300px] w-full">
       <Button
-        variant={"secondary"}
+        variant="secondary"
         size="icon"
         className="absolute left-4 top-4 z-50 rounded-full"
         onClick={handleBackClick}
